75c2990cdbbf76b9607c4c928a835416ea8ce926
Bishajit Lodh <lodh@Bishajits-MacBook-Pro.local>
2023-11-09 17:14:00 -0500

    Readme commit

---
+++ src/main/java/creditSystem/creditCard.java
@@ -0,0 +1,9 @@
+package creditSystem;
+public class creditCard {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        item banana = new item();
+
+    }
+}
---
+++ src/main/java/creditSystem/item.java
@@ -0,0 +1,9 @@
+package creditSystem;
+
+public class item {
+    public String name = "banana";
+    public item() {
+        System.out.println("A " + name + " item object is created");
+    }
+
+}
